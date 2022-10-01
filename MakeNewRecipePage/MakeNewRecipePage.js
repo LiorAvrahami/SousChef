2a0d74dfddd9a2d6e0588f3ed30fa53673bb64b3
LiorAvrahami <lior.2pi@gmail.com>
2022-10-02 01:15:58 +0300

    MakeNewRecipePage now works
    also made styling improvments
    
    Signed-off-by: LiorAvrahami <lior.2pi@gmail.com>

--- MakeNewRecipePage/MakeNewRecipePage.js
+++ MakeNewRecipePage/MakeNewRecipePage.js
@@ -10,3 +10,13 @@ async function onImportClicked() {
 
     alert(text)
 }
+
+function SubmitRecipe() {
+    var name = document.getElementById("recipe_name_field").value;
+    var steps = document.getElementById("recipe_steps_field").value.split("\n");
+
+    var recipes = load_recipes_json_Object();
+    recipes.push({ "name": name, "steps": steps })
+    save_recipes_json_Object(recipes);
+    SwitchPage("recipesOverviewPage")
+}
